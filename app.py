d526935ebf07f86ae9e68cee8fba020d5e66165c
KothaSathvika <sathvikakotha910@gmail.com>
2023-06-10 20:55:26 -0400

    Basic API

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/post_json', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        response = {"greeting": "Hi " + json["name"]}
+        return response
+    else:
+        return 'Content-Type not supported!'
+    
+app.run(host='0.0.0.0', port=5000)
